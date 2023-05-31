2f7f291f949ba092dbfad42fa4313e0f7225da96
alvaro <alvaro@ED>
2023-05-31 08:52:33 +0200

    commit1

--- ejemplo1.java
+++ ejemplo1.java
@@ -1,8 +1,8 @@
 public class ejemplo1 {
 
 	public static void main(String[] args) {		
-		System.out.println("Holame llamo rama 2");
-		System.out.println(" Hola Soy la rama 1");
+		System.out.println("cambio de la rama 1");
+		System.out.println(" holaa");
 	}
 
 }

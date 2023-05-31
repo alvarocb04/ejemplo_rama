0fabe81eda6ebc7501f9bd23f1f571c54768c716
alvarocb04 <131864542+alvarocb04@users.noreply.github.com>
2023-05-31 09:37:56 +0200

    Update ejemplo1.java

--- ejemplo1.java
+++ ejemplo1.java
@@ -1,7 +1,7 @@
 public class ejemplo1 {
 
 	public static void main(String[] args) {		
-		System.out.println("Hola soy chico");
+		System.out.println("Hola soy alvaro");
 		int x=0;
 		System.out.println("Hola soy chico pero el original ");
 	}

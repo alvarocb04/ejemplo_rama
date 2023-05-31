6d92b4b32c89bdf60ad1b27307b29c85b3028c21
alvarocb04 <131864542+alvarocb04@users.noreply.github.com>
2023-05-31 09:37:00 +0200

    Ddick (#9)
    * chico (#7)
    
    * commit

--- ejemplo1.java
+++ ejemplo1.java
@@ -1,6 +1,8 @@
 public class ejemplo1 {
 
 	public static void main(String[] args) {		
+		System.out.println("Hola soy chico");
+		int x=0;
 		System.out.println("Hola soy chico pero el original ");
 	}
 
